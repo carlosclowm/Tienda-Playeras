398649b0d9f67a277560542a4507d549b54134fb
Carlos Serrano <carlosclowm@gmail.com>
2018-07-19 19:06:48 -0500

    Borrado codigo de prueba

--- apps/almacen/urls.py
+++ apps/almacen/urls.py
@@ -4,11 +4,9 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', index, name='index'),
     path('/nuevo', login_required(AlmacenCreate.as_view()), name='almacen_crear'),
     path('/lista', login_required(AlmacenList.as_view()), name='almacen_lista'),
     path('/editar/<int:pk>/', login_required(AlmacenUpdate.as_view()), name='almacen_editar'),
     path('/eliminar/<int:pk>/', login_required(AlmacenDelete.as_view()), name='almacen_eliminar'),
-    path('/listado', listado, name='listado'),
     ]
 app_name = 'almacen'
--- apps/almacen/views.py
+++ apps/almacen/views.py
@@ -8,46 +8,7 @@ from apps.almacen.forms import AlmacenForm
 from apps.almacen.models import Playeras
 # Create your views here.
 
-def listado(request):
-	lista = serializers.serialize('json', User.objects.all(), fields=['usarname', 'first_name'])
-	return HttpResponse(lista, content_type='application/json')
 
-def index(request):
-	return HttpResponse("almacen")
-
-def almacen_view(request):
-	if request.method == 'POST':
-		form = AlmacenForm(request.POST)		
-		if form.is_valid():
-			form.save()
-		return redirect('almacen:lista')
-	else:
-		form = AlmacenForm()
-
-	return render(request, 'almacen/almacen_form.html', {'form':form})
-
-def almacen_list(request):
-	playera = Playeras.objects.all().order_by('id')
-	contexto = {'almacen': playera}
-	return render(request, 'almacen/almacen_list.html', contexto)
-
-def almacen_edit(request, id_playera):
-	playera = Playeras.objects.get(id=id_playera)
-	if request.method == 'GET':
-		form = AlmacenForm(instance=playera)
-	else:
-		form = AlmacenForm(request.POST, instance=playera)
-		if form.is_valid():
-			form.save()
-			return redirect('almacen:almacen_lista')
-	return render(request, 'almacen/almacen_form.html', {'form':form})
-
-def almacen_delete(request, id_playera):
-	playera = Playeras.objects.get(id=id_playera)
-	if request.method == 'POST':
-		playera.delete()
-		return redirect('almacen:almacen_lista')
-	return render(request, 'almacen/almacen_delete.html', {'playera':playera})
 
 class AlmacenList(ListView):
 	model = Playeras
